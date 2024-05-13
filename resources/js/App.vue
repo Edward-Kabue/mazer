39087a76cdf6dcbe2c08758c76242121e8a88762
Edward-Kabue <ekabue0@gmail.com>
2024-05-13 11:22:01 +0300

    feat: Add new routes and components for Home and Test pages

---
+++ resources/js/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <router-view v-slot="{ Component, route }">
+        <div :key="route.name">
+            <Component :is="Component" />
+        </div>
+    </router-view>
+    </div>
+ 
+</template>
